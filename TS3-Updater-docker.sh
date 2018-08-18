2ace996dc43bf158e35c23438e2d39ab0936db2d
Jacob Saleh <jacobsaleh@gmail.com>
2018-08-18 20:03:52 +0300

    Update TS3-Updater-docker.sh

--- TS3-Updater-docker.sh
+++ TS3-Updater-docker.sh
@@ -2,7 +2,7 @@
 INSTCOMM="yum"
 if ! command -v curl &> /dev/null ; then sudo $INSTCOMM install curl -y; fi
 TS_New_Version=$(curl -s https://www.teamspeak.com/en/downloads.html#server | grep teamspeak3-server_linux_amd64- | grep -m 1 -o -P '(?<=amd64-).*(?=.tar.bz2)')
-TS_Current_Version=$(docker exec centos-ts3 cat /var/log/teamspeak/CurrentTSversion)
+TS_Current_Version=$(sudo docker exec centos-ts3 cat /var/log/teamspeak/CurrentTSversion)
 echo "Cloud Version = $TS_New_Version"
 echo "Current installed version $TS_Current_Version"
 if [ $TS_New_Version = $TS_Current_Version ]
@@ -16,18 +16,18 @@ if [ $TS_New_Version = $TS_Current_Version ]
 		echo "Downloading version $TS_New_Version"
 		wget http://teamspeak.gameserver.gamed.de/ts3/releases/$TS_New_Version/teamspeak3-server_linux_amd64-$TS_New_Version.tar.bz2
 		TS3CONTAINER="centos-ts3"
-		TS3USER=$(docker exec $TS3CONTAINER grep User /var/log/teamspeak/Details | cut -d "=" -f 2)
-		TS3PATH=$(docker exec $TS3CONTAINER grep Path /var/log/teamspeak/Details | cut -d "=" -f 2)
+		TS3USER=$(sudo docker exec $TS3CONTAINER grep User /var/log/teamspeak/Details | cut -d "=" -f 2)
+		TS3PATH=$(sudo docker exec $TS3CONTAINER grep Path /var/log/teamspeak/Details | cut -d "=" -f 2)
 		echo $TS3USER;echo $TS3PATH
 		tar xvf teamspeak3-server_linux_amd64-$TS_New_Version.tar.bz2
-		docker stop $TS3CONTAINER
-		docker cp ./teamspeak3-server_linux_amd64 $TS3CONTAINER:/opt/teamspeak3server/
-		docker start $TS3CONTAINER
+		sudo docker stop $TS3CONTAINER
+		sudo docker cp ./teamspeak3-server_linux_amd64 $TS3CONTAINER:/opt/teamspeak3server/
+		sudo docker start $TS3CONTAINER
 		sleep 1
-		docker exec $TS3CONTAINER chown -R $TS3USER: $TS3PATH
+		dsudo ocker exec $TS3CONTAINER chown -R $TS3USER: $TS3PATH
 		echo $TS_New_Version > CurrentTSversion
-		docker cp CurrentTSversion $TS3CONTAINER:/var/log/teamspeak/CurrentTSversion
-		docker exec $TS3CONTAINER kill -9 1
+		sudo docker cp CurrentTSversion $TS3CONTAINER:/var/log/teamspeak/CurrentTSversion
+		sudo docker exec $TS3CONTAINER kill -9 1
 		exit 0
 fi
 exit 255
